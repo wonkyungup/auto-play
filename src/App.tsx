121d125105d6a9bfbeca6e0be8d05076f33171b6
wonkyungup <jack@netsarang.com>
2023-01-28 00:09:27 +0900

    add: material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import AccountCircleSharpIcon from '@mui/icons-material/AccountCircleSharp';
 
 const App = () => {
   return (
@@ -18,6 +21,12 @@ const App = () => {
         >
           Learn React
         </a>
+        <AccountCircleSharpIcon />
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
       </header>
     </div>
   );
