ae16237539419edd6721158450bc4541838a4af1
KyungUp Won <jack@netsarang.com>
2023-02-03 18:03:01 +0900

    check url

--- src/assets/ts/content.ts
+++ src/assets/ts/content.ts
@@ -1,3 +1,15 @@
+import Defs from './constants';
+
 chrome.runtime.onMessage.addListener((req) => {
-    console.log(req);
+    switch (req.action) {
+        case Defs.ACTION_ALERT:
+            alert(req.message);
+            break
+        case Defs.ACTION_ENABLE:
+            console.log('1')
+            console.log(req.message);
+            break
+        default:
+            break
+    }
 })
