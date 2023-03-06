a85c2ed5a2d01c17410b379229708828ff8f8c01
wonkyungup <jack@netsarang.com>
2023-03-06 13:04:20 +0900

    add: Option ContextMenu

--- src/assets/tabs.ts
+++ src/assets/tabs.ts
@@ -1,41 +1,94 @@
 import Browser from 'webextension-polyfill';
+import Defs from "./constants";
+import DB from "../model";
+
+const db = new DB();
 
 export default class Tabs {
-     static async getTabInfo (id: number) {
+    isValidToYoutubeShort (url: string) {
+        return url.includes(Defs.URI_YOUTUBE_SHORTS);
+    };
+
+    async getTabInfo (id: number) {
         return await Browser.tabs.get(<number>id);
     };
 
-    static onClickIconTab (cb: (args: any) => void) {
+    onClickIconTab () {
         Browser.action.onClicked.addListener(async ({id}) => {
             if (Browser.runtime.lastError) {
                 console.error(Browser.runtime.lastError);
             }
 
-            cb(await Tabs.getTabInfo(<number>id));
+            try {
+                const tab = await this.getTabInfo(<number>id);
+                if (!this.isValidToYoutubeShort(<string>tab.url) && !await db.getStateIconSync()) await Browser.tabs.sendMessage(<number>tab.id, Defs.URI_ERROR);
+                else {
+                    await db.toggleStateIcon();
+
+                    if (await db.getActiveTabSync() && !await db.getStateIconSync()) {
+                        return await this.onWatchTab();
+                    }
+
+                    await db.setActiveTab(<number>tab.id);
+                    await Browser.tabs.sendMessage(<number>tab.id, Defs.STR_YOUTUBE_SHORTS);
+                }
+            } catch (err) {
+                console.log(err);
+            }
         });
     };
 
-    static onActivatedTab (cb: () => void) {
-        Browser.tabs.onActivated.addListener(() => {
+    onActivatedTab () {
+        Browser.tabs.onActivated.addListener(async () => {
             if (Browser.runtime.lastError) {
                 console.error(Browser.runtime.lastError);
             }
 
-            cb();
+            await this.onWatchTab();
         });
     };
 
-    static onUpdatedTab (cb: () => void) {
-        Browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
+    onUpdatedTab () {
+        Browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
             if (Browser.runtime.lastError) {
                 console.error(Browser.runtime.lastError);
             }
 
-            if (changeInfo.url) cb();
+            if (changeInfo.url) {
+                await this.onWatchTab();
+            }
         });
     };
 
-    static async getAllTabSync () {
+    async getAllTabSync () {
         return await Browser.tabs.query({});
+    }
+
+    async onWatchTab () {
+        const tabId = await db.getActiveTabSync();
+        try {
+            if (tabId) {
+                const tabs = <any[]>await this.getAllTabSync();
+                const activeTab = tabs.filter((info: { id: number }) => info?.id === Number(tabId));
+
+                if (activeTab.length <= 0) await db.disabled();
+                else {
+                    const { id, url } = activeTab[0];
+                    if (!this.isValidToYoutubeShort(url) && await db.getStateIconSync()) {
+                        await db.disabled();
+                    }
+
+                    await Browser.tabs.sendMessage(id, Defs.STR_YOUTUBE_SHORTS);
+                }
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    onHandlerTabs () {
+       this.onClickIconTab();
+       this.onActivatedTab();
+       this.onUpdatedTab();
     }
 };
