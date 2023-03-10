a01c5a29b697543071ac5ad587ac4f774d6f4ce8
wonkyungup <jack@netsarang.com>
2023-03-11 00:16:29 +0900

    modify: media play remove, toggle switch in video

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,24 @@
 import Browser from "webextension-polyfill";
 
 const URL_YOUTUBE_SHORTS = 'https://www.youtube.com/shorts';
-const isValidToUrl = (url: string) => url.includes(URL_YOUTUBE_SHORTS) ;
+const isValidToUrl = (url: string) => url.includes(URL_YOUTUBE_SHORTS);
 
 Browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
     try {
         if (Browser.runtime.lastError) console.error(Browser.runtime.lastError);
-        if (isValidToUrl(<string>changeInfo.url)) {
-            await Browser.tabs.sendMessage(tabId, `URL: Detection`);
+        else if (changeInfo.url && isValidToUrl(changeInfo?.url)) {
+            await Browser.tabs.sendMessage(tabId, 'URL: Detection');
         }
     } catch (err) {
-        console.log(err);
+        console.log(`TAB :: UPDATE :: ERROR :: ${err}`);
     }
 });
+
+Browser.runtime.onMessage.addListener(async (request, sender) => {
+    if (request === 'URL: Detection') {
+        const tabId = sender.tab?.id;
+        if (tabId) {
+            await Browser.tabs.sendMessage(tabId, 'URL: Detection');
+        }
+    }
+})
--- src/content.tsx
+++ src/content.tsx
@@ -1,83 +1,54 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SystemTheme from './components/SystemTheme';
+import ToggleSwitch from "./components/ToggleSwitch";
 import Browser from "webextension-polyfill";
 import YoutubeShorts from "./assets/youtubeShorts";
 
 const youtubeShorts = new YoutubeShorts('shorts-container', 'shorts-inner-container');
-setTimeout(async () => {
-    await youtubeShorts.setCurPlayVideo();
 
-    const actions = youtubeShorts._innerContainer?.querySelector('#actions');
-    const div = document.createElement('div');
-    div.id = 'auto-play';
-    actions?.append(div);
-
-    const autoPlay = document.getElementById('auto-play');
-    if (autoPlay) {
-        ReactDOM.render(
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>,
-            autoPlay
-        )
-    }
-}, 300);
-
-const MediaControlCard = () => {
-    const theme = useTheme();
-    const [profile, setProfile] = React.useState();
-    const initVideoProfile = async () => {
-        setTimeout(async () => {
-            // @ts-ignore
-            setProfile(await youtubeShorts.getCurPlayVideoProfile());
-        }, 200);
-    }
-
-    React.useEffect(() => {
-        (async () => await initVideoProfile())();
-        Browser.runtime.onMessage.addListener(async message => {
-            if (message === `URL: Detection`) {
-                await initVideoProfile();
-            }
-        })
-    }, [])
-
-    return (
-        <Card sx={{ display: 'flex', right: 0, position: 'absolute', top: 0 }}>
-            <CardMedia
-                component="img"
-                sx={{ width: 48, height: 48 }}
-                image={profile}
-                alt="load"
-            />
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <IconButton aria-label="previous" sx={{ display: 'none' }}>
-                    {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-                </IconButton>
-                <IconButton aria-label="play/pause">
-                    <PlayArrowIcon sx={{ width: 32, height: 32 }}/>
-                </IconButton>
-                <IconButton aria-label="next" sx={{ display: 'none' }}>
-                    {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-                </IconButton>
-            </Box>
-        </Card>
-    );
+window.onload = async () => {
+    await Browser.runtime.sendMessage('URL: Detection');
 }
 
 const App = () => {
+    React.useEffect(() => {
+        console.log(youtubeShorts._innerContainer);
+    }, [])
+
     return (
         <SystemTheme>
-            <MediaControlCard />
+            <ToggleSwitch />
         </SystemTheme>
     )
 }
+
+
+Browser.runtime.onMessage.addListener((request) => {
+    if (request === 'URL: Detection') {
+        setTimeout(async () => {
+            await youtubeShorts.setCurPlayVideo();
+
+            const actions = youtubeShorts._innerContainer?.querySelector('ytd-shorts-player-controls');
+            if (actions?.querySelector('#auto-youtube-shorts-scroll-down') === null) {
+                const div = document.createElement('div');
+                div.id = 'auto-youtube-shorts-scroll-down';
+                div.style.position = 'absolute';
+                div.style.top = '10.5%';
+                div.style.right = '10%';
+
+                actions?.append(div);
+            }
+
+            const autoYoutubeShortsScrollDown = actions?.querySelector('#auto-youtube-shorts-scroll-down');
+            if (autoYoutubeShortsScrollDown) {
+                ReactDOM.render(
+                    <React.StrictMode>
+                        <App />
+                    </React.StrictMode>,
+                    autoYoutubeShortsScrollDown
+                )
+            }
+        }, 500);
+    }
+})
