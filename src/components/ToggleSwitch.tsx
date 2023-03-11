0c65b2a4a08e6bab961ecdc4b737694fdf3bfe12
wonkyungup <jack@netsarang.com>
2023-03-11 23:57:51 +0900

    modify

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -4,24 +4,20 @@ import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
 import Tooltip from '@mui/material/Tooltip';
 
-const ToggleSwitch = () => {
-    const [checked, setChecked] = React.useState<boolean>(false);
-    const handlerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-    }
-
+const ToggleSwitch = (props: { checked: boolean | undefined; onChange: ((event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void) | undefined; }) => {
     return (
         <Tooltip
-            title={checked ? 'stop autoplay' : 'autoplay settings'}
+            title={props.checked ? 'stop autoplay' : 'autoplay settings'}
             arrow
         >
             <Switch
+                id="auto-youtube-shorts-player"
                 sx={{ pointerEvents: 'all' }}
                 checkedIcon={<PlayCircleFilledIcon fontSize="large" />}
                 icon={<PauseCircleFilledIcon fontSize="large" />}
                 size="medium"
-                checked={checked}
-                onChange={handlerChange}
+                checked={props.checked}
+                onChange={props.onChange}
             />
         </Tooltip>
     );
