ca28f3bed02bd51b8cd551b69c190a7c074e8bc7
wonkyungup <jack@netsarang.com>
2023-03-20 20:03:48 +0900

    modify: scroll

--- src/assets/youtubeShorts.ts
+++ src/assets/youtubeShorts.ts
@@ -1,19 +1,23 @@
 import Utils from './utils';
 import $ from 'jquery';
+import Browser from 'webextension-polyfill';
+import Defs from './constatns';
 
 export default class YoutubeShorts {
   _innerContainer: Element | null;
   _innerList: any[];
   _innerVideo: any;
+  _innerPlayerControl: any;
   constructor() {
     this._innerContainer = null;
     this._innerList = [];
     this._innerVideo = null;
+    this._innerPlayerControl = null;
   }
 
   async waitForVideoContainer() {
     const video: any = await Utils.waitForElement('video');
-    await Utils.sleep(300);
+    await Utils.sleep(500);
 
     this._innerVideo = video || null;
     this._innerList =
@@ -26,10 +30,15 @@ export default class YoutubeShorts {
         (element) => $(element).attr('is-active') !== undefined,
       )[0] || null;
 
+    this._innerPlayerControl = this._innerContainer?.querySelector(
+      'ytd-shorts-player-controls',
+    );
     return;
   }
 
   async getNextElement() {
+    await this.waitForVideoContainer();
+
     const index = this._innerList.indexOf(this._innerContainer);
     if (index > 0) {
       return this._innerList[index + 1];
@@ -37,16 +46,15 @@ export default class YoutubeShorts {
   }
 
   doesLoopVideo() {
-    const video = this._innerContainer?.querySelector('video');
-    video?.setAttribute('loop', String(true));
+    this._innerVideo?.setAttribute('loop', String(true));
   }
 
   doesNextVideo() {
-    const video = this._innerContainer?.querySelector('video');
-    video?.removeAttribute('loop');
-    video?.addEventListener('ended', async () => {
+    this._innerVideo?.removeAttribute('loop');
+    this._innerVideo.onended = async () => {
       const element = await this.getNextElement();
       element.scrollIntoView({ block: 'end', behavior: 'smooth' });
-    });
+      await Browser.runtime.sendMessage({ event: Defs.EVENT_PAGE_UPDATE });
+    };
   }
 }
--- src/content.tsx
+++ src/content.tsx
@@ -8,8 +8,6 @@ import YoutubeShorts from './assets/youtubeShorts';
 import EventsListener from './assets/eventsListener';
 import ToggleSwitch from './components/ToggleSwitch';
 
-let isAutoPlay: boolean = false;
-
 EventsListener.onReload();
 EventsListener.onKeyup();
 EventsListener.onShortUpDownButton();
@@ -18,13 +16,21 @@ EventsListener.onWheelRemove();
 const youtubeShorts = new YoutubeShorts();
 
 const Switch = () => {
-  const [checked, setChecked] = React.useState(isAutoPlay);
+  const [checked, setChecked] = React.useState(false);
   const handlerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    isAutoPlay = event.target.checked;
-    setChecked(isAutoPlay);
+    setChecked(event.target.checked);
+    Browser.storage.local
+      .set({ isAutoPlay: event.target.checked })
+      .then(() => {});
   };
 
   React.useEffect(() => {
+    const checkStorageData = async () => {
+      return await Browser.storage.local.get(['isAutoPlay']);
+    };
+
+    checkStorageData().then((r) => setChecked(r.isAutoPlay));
+
     if (checked) {
       youtubeShorts.doesNextVideo();
     } else {
@@ -44,40 +50,21 @@ Browser.runtime.onMessage.addListener(async ({ event }) => {
   if (event === Defs.EVENT_PAGE_UPDATE || event === Defs.EVENT_PAGE_RELOAD) {
     await youtubeShorts.waitForVideoContainer();
     console.log(youtubeShorts);
-    // await Utils.onWaitForElement('#shorts-inner-container');
-    //
-    // if ($('#shorts-inner-container').length > 0) {
-    //   await youtubeShorts.onReady(
-    //     '#shorts-container',
-    //     '#shorts-inner-container',
-    //   );
-    //
-    //   const shortsContainer = youtubeShorts._shortsContainer;
-    //   const innerContainer = youtubeShorts._innerContainer;
-    //
-    //   // console.log(shortsContainer);
-    //   // console.log(innerContainer);
-    //   if (innerContainer === null) {
-    //     await Browser.runtime.sendMessage({ event: Defs.EVENT_PAGE_RELOAD });
-    //   }
-    //
-    //   if (innerContainer !== null) {
-    //     const autoYoutubeShortsScrollDown = $(
-    //       '#auto-youtube-shorts-scroll-down',
-    //     );
-    //     if (autoYoutubeShortsScrollDown.length <= 0) {
-    //       $(innerContainer)
-    //         .parent()
-    //         .append(
-    //           '<div id="auto-youtube-shorts-scroll-down" style="display: inline-block;position: relative;" ></div>',
-    //         );
-    //
-    //       ReactDOM.render(
-    //         <Switch />,
-    //         document.getElementById('auto-youtube-shorts-scroll-down'),
-    //       );
-    //     }
-    //   }
-    // }
+
+    const autoYoutubeShortsScrollDown = $('#auto-youtube-shorts-scroll-down');
+    if (autoYoutubeShortsScrollDown.length > 0) {
+      autoYoutubeShortsScrollDown.remove();
+    }
+
+    $(youtubeShorts._innerPlayerControl)
+      .children('yt-icon-button:eq(1)')
+      .before(
+        '<div id="auto-youtube-shorts-scroll-down" style="display: inline-block;position: relative;" ></div>',
+      );
+
+    ReactDOM.render(
+      <Switch />,
+      document.getElementById('auto-youtube-shorts-scroll-down'),
+    );
   }
 });
