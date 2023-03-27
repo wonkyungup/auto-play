f94c774826fa5bb747cfdf6ecba7afbf76c9213d
wonkyungup <jack@netsarang.com>
2023-03-27 17:24:28 +0900

    add: option button, tooltip

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import { useTranslation } from 'react-i18next';
 
 const ToggleSwitch = ({
   isSwitch,
@@ -7,11 +9,20 @@ const ToggleSwitch = ({
   isSwitch: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
+  const { t } = useTranslation();
+
   return (
-    <label id="auto-play-switch" className="switch">
-      <input type="checkbox" checked={isSwitch} onChange={onChange} />
-      <span className="slider round"></span>
-    </label>
+    <Tooltip
+      title={
+        isSwitch ? t('tooltip:enableAutoPlay') : t('tooltip:disableAutoPlay')
+      }
+      arrow
+    >
+      <label id="auto-play-switch" className="switch">
+        <input type="checkbox" checked={isSwitch} onChange={onChange} />
+        <span className="slider round"></span>
+      </label>
+    </Tooltip>
   );
 };
 
