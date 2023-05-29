9bc30ca4a947b890cf8da8163f201a0406dda1c8
wonkyungup <jack@netsarang.com>
2023-05-29 13:56:20 +0900

    add: Download Link

--- src/components/DownloadLink.tsx
+++ src/components/DownloadLink.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
-import SettingsIcon from '@mui/icons-material/Settings';
+import BrowserUpdatedIcon from '@mui/icons-material/BrowserUpdated';
 import Tooltip from '@mui/material/Tooltip';
 import { useTranslation } from 'react-i18next';
 
-interface TypeButton {
-  rotate: number;
-  onClick: () => void;
-}
-
-const Button = ({ rotate, onClick }: TypeButton) => {
+const DownloadLink = () => {
   const { t } = useTranslation();
 
   return (
-    <Tooltip title={t('tooltip:setting')} arrow>
+    <Tooltip title={t('tooltip:downloadLinkOpen')} arrow>
       <IconButton
         aria-label="option"
         sx={{
@@ -22,16 +17,21 @@ const Button = ({ rotate, onClick }: TypeButton) => {
           bottom: 9,
           top: 0,
           right: 0,
-          left: 100,
-          transform: `rotate(${rotate}deg)`,
+          left: 72,
           color: '#ffffff',
         }}
-        onClick={onClick}
+        onClick={() => {
+          window.open(
+            window.location.href.replace('youtube.com', 'ssyoutube.com'),
+            '_blank',
+            'height=700,width=700',
+          );
+        }}
       >
-        <SettingsIcon sx={{ width: 24, height: 24 }} />
+        <BrowserUpdatedIcon sx={{ width: 24, height: 24 }} />
       </IconButton>
     </Tooltip>
   );
 };
 
-export default Button;
+export default DownloadLink;
